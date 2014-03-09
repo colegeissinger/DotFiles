63a20d9f1f47dc0127d271084dca3e876cfddcb5
Cole Geissinger <colegeissinger@coles-mbp.hsd1.ca.comcast.net>
2014-03-08 21:44:04 -0800

    Fixing some invalid file paths and removing some unneeded scripts

--- install-deps.sh
+++ install-deps.sh
@@ -23,9 +23,9 @@ ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
 # https://github.com/rupa/z
 # z, oh how i love you
-cd ~/code
+cd ~
 git clone https://github.com/rupa/z.git
-chmod +x ~/code/z/z.sh
+chmod +x ~/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
 
 # z binary is already referenced from .bash_profile
@@ -35,11 +35,6 @@ chmod +x ~/code/z/z.sh
 # Spotify Controll Script
 cd ~/code
 git clone git://github.com/dronir/SpotifyControl.git
-
-# https://github.com/jeroenbegyn/VLCControl
-# VLC Controll Script
-cd ~/code
-git clone git://github.com/jeroenbegyn/VLCControl.git
 
 
 # my magic photobooth symlink -> dropbox. I love it.
