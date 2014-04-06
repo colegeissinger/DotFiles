36e827412782fe52a99578039549d964e1b1fd22
Cole Geissinger <colegeissinger@gmail.com>
2014-04-05 17:49:20 -0700

    Rearranging and cleaning up un-needed code

--- install-deps.sh
+++ install-deps.sh
@@ -15,41 +15,21 @@ curl -L https://get.rvm.io | bash -s stable --ruby
 npm install -g nave
 
 
-# homebrew!
-# you need the code CLI tools YOU FOOL.
+# homebrew! you need the code CLI tools YOU FOOL.
 ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
-
-
-# https://github.com/rupa/z
-# z, oh how i love you
+# z, oh how i love you - https://github.com/rupa/z
 cd ~
 git clone https://github.com/rupa/z.git
 chmod +x ~/z/z.sh
-# also consider moving over your current .z file if possible. it's painful to rebuild :)
 
-# z binary is already referenced from .bash_profile
-
-
-# https://github.com/dronir/SpotifyControl
-# Spotify Controll Script
+# Spotify Control Script for Terminal - https://github.com/dronir/SpotifyControl
 cd ~
 git clone git://github.com/dronir/SpotifyControl.git
 
-
-# my magic photobooth symlink -> dropbox. I love it.
-# first move Photo Booth folder out of Pictures
-# then start Photo Booth. It'll ask where to put the library.
-# put it in Dropbox/public
-
-# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# then you grab public URL and send off your video message in a heartbeat.
-
-
-# for the c alias (syntax highlighted cat)
+# For the c alias (syntax highlighted cat)
 sudo easy_install Pygments
 
-
-# chrome canary as default
-# on a mac you can set chrome canary as your default inside of Safari preferences :)
-
+# Add the ~/Sites directory
+cd ~
+mkdir Sites
